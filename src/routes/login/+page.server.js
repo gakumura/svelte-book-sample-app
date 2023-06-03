21102b1b938e6ccac08d92706d854e4fb4534994
Oracle Public Cloud User <opc@gnakamur-remote-dev.sub11050747250.vcngaku1.oraclevcn.com>
2023-06-03 03:09:58 +0000

    Added Auth0 login

---
+++ src/routes/login/+page.server.js
@@ -0,0 +1,25 @@
+import { fail } from '@sveltejs/kit';
+import { sendPasswordlessLink } from '$lib/server/auth0';
+
+export const actions = {
+    default: async ({ cookies, request, url }) => {
+        const data = await request.formData();
+        const email = data.get('email');
+
+        if (!email) {
+            return fail(400, { email, error: 'missing' });
+        }
+
+        // 簡易的なフォーマットチェック、実際はもっとちゃんとやるべし
+        if (!/^.+@.+$/.test(email)) {
+            return fail(400, { email, error: 'invalid_format' });
+        }
+
+        const state = crypto.randomUUID();
+        const redirectUri = `${url.origin}/api/auth/callback`;
+        await sendPasswordlessLink(email, state, redirectUri);
+
+        cookies.set('state', state, { path: '/' });
+        return { success: true };
+    }
+};
